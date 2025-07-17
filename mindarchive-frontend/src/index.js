0ecb10791d8f896f87c403a57e1083973a8a5e98
Justin Asarias <01Justin23@gmail.com>
2025-07-16 17:29:15 -0700

    Index.js code cleanup

--- mindarchive-frontend/src/index.js
+++ mindarchive-frontend/src/index.js
@@ -1,4 +1,5 @@
 // src/index.js
+import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -7,6 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from './ThemeContext'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
